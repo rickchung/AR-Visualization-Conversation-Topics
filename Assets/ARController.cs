26966f0e4477e9b6e81502e55e9430946352402b
rickchung <rickjung88@gmail.com>
2019-03-27 12:15:40 -0700

    Initial commit

---
+++ Assets/ARController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARController : MonoBehaviour {
+
+    public GameObject ARCamera;
+    
+	void Start() 
+    {
+        ARCamera.SetActive(false);	
+	}
+
+    public void ToggleARCamera()
+    {
+        ARCamera.SetActive(!ARCamera.activeSelf);
+    }
+
+}
---
+++ Assets/VoiceController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceController : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ToggleMicrophone()
+    {
+        string micName = null;
+        if (Microphone.devices.Length > 0)
+        {
+            micName = Microphone.devices[0];
+        }
+
+        if (!Microphone.IsRecording(micName))
+        {
+            Debug.Log("[LOG] Start recording at " + micName);
+            StartMicrophone(micName);
+        }
+        else
+        {
+            Debug.Log("[LOG] Stop recording at " + micName);
+            StopMicrophone(micName);
+        }
+    }
+
+    private void StartMicrophone(string micName)
+    {
+        int clipSizeSec = 3;
+        StartCoroutine(CheckMic(micName, clipSizeSec));
+    }
+
+    private IEnumerator CheckMic(string micName, int clipSizeSec)
+    {
+        int samplingRate = 44100;
+        int latency = 0;
+
+        // Start the microphone
+        audioSource.clip = Microphone.Start(micName, true, clipSizeSec, samplingRate);
+        audioSource.loop = true;
+
+        // For real-time playback, set latency to 0.
+        while (!(Microphone.GetPosition(micName) > latency)) { }
+        audioSource.Play();
+
+        while (Microphone.IsRecording(micName))
+        {
+            yield return new WaitForSeconds(clipSizeSec);
+            // Save the clip after waiting
+            if (audioSource.clip != null)
+            {
+                SaveMicFile(audioSource.clip);
+            }
+        }
+    }
+
+    private void StopMicrophone(string micName)
+    {
+        // Stop the audio source is it's playing
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+        // Stop the microphone (this will stop the coroutine)
+        Microphone.End(micName);
+        // Clear the audio source
+        audioSource.clip = null;
+    }
+
+    private void SaveMicFile(AudioClip clip)
+    {
+        // Get current date time as file name
+        string timestamp = DateTime.Now.ToString();
+        timestamp = timestamp.Replace('/', '-').Replace(':', '-').Replace(' ', '-');
+        string micFilename = "MicAudio-" + timestamp;
+
+        // Save the clip
+        SavWav.Save(micFilename, clip);
+    }
+}
