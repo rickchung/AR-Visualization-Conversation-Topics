e9e5992406ec38adb0ee4a0d975abb3bb62bb27c
rickchung <rickjung88@gmail.com>
2019-04-25 15:15:59 -0700

    Set focus mode of the camera

---
+++ Assets/CameraFocusControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CameraFocusControl : MonoBehaviour
+{
+    void Start()
+    {
+        bool focusModeSet = CameraDevice.Instance.SetFocusMode(
+            CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        if (!focusModeSet)
+        {
+            Debug.Log("Failed to set focus mode.");
+        }
+    }
+}
