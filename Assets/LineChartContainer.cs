0055a23c32d11575a0b02ab3129569bc8a0e3a24
rickchung <rickjung88@gmail.com>
2019-08-09 18:00:00 -0700

    Add skeleton of charts

---
+++ Assets/LineChartContainer.cs
@@ -0,0 +1,52 @@
+﻿// LineChartContainer.cs
+// Reference: https://www.youtube.com/watch?v=CmU5-v-v1Qo&list=PLzDRvYVwl53v5ur4GluoabyckImZz3TVQ&index=3&t=75s
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineChartContainer : MonoBehaviour
+{
+    public Sprite dotImage;
+    private RectTransform chartContainer;
+    private Vector2 dotSize = new Vector2(12, 12);
+    private float xSize = 22f;
+    private float yLimMax = 100f;
+    private float chartHeight;
+
+    private void Awake()
+    {
+        chartContainer = GetComponent<RectTransform>();
+        chartHeight = chartContainer.sizeDelta.y;
+        Debug.Log(chartHeight);
+
+        List<float> data = new List<float> { 1, 21, 51, 61, 71, 91, 61, 31, };
+        RenderData(data);
+    }
+
+    private void RenderData(List<float> data)
+    {
+        for (int i = 0; i < data.Count; i++)
+        {
+            Vector2 position = new Vector2(
+                xSize + i * xSize,
+                (data[i] / yLimMax) * chartHeight
+            );
+
+            RenderDot(position);
+        }
+    }
+
+    private void RenderDot(Vector2 anchoredPosition)
+    {
+        GameObject newDot = new GameObject("dot", typeof(Image));
+        newDot.transform.SetParent(chartContainer);
+        newDot.GetComponent<Image>().sprite = dotImage;
+        RectTransform dotRectTransform = newDot.GetComponent<RectTransform>();
+        dotRectTransform.anchoredPosition = anchoredPosition;
+        dotRectTransform.sizeDelta = dotSize;
+        dotRectTransform.anchorMin = new Vector2(0, 0);
+        dotRectTransform.anchorMax = new Vector2(0, 0);
+    }
+}
--- Assets/StatChartController.cs
+++ Assets/StatChartController.cs
@@ -9,19 +9,18 @@ public class StatChartController : MonoBehaviour
     public Transform panChartNumPhases;
     public Transform panChartNumWords;
 
-    private Transform nPhaseUpdateLoc, nWordUpdateLoc;
-
+    private TMPro.TextMeshProUGUI valTxtNumPhases, valTxtNumWords;
+    private LineChartContainer chartContainerNumWords;
 
     void Start()
     {
-        nPhaseUpdateLoc = panChartNumPhases.Find("PhText");
-        nWordUpdateLoc = panChartNumWords.Find("PhText");
+        valTxtNumPhases = panChartNumPhases.Find("PhText").GetComponent<TMPro.TextMeshProUGUI>();
+        valTxtNumWords = panChartNumWords.Find("PhText").GetComponent<TMPro.TextMeshProUGUI>();
     }
 
     public void UpdateNumPhaseChart(int localNum, int remoteNum = -1)
     {
-        nPhaseUpdateLoc.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = localNum + " ps";
-
+        valTxtNumPhases.text = localNum + " ps";
     }
 
     public void UpdateNumWordChart(int localNumWords, int localNumPhases, int remoteNumWords = -1, int remoteNumPhases = -1)
@@ -29,11 +28,8 @@ public class StatChartController : MonoBehaviour
         try
         {
             float localAvg = localNumWords / localNumPhases;
-            nWordUpdateLoc.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = localAvg.ToString("0.0") + " ws/ps";
+            valTxtNumWords.text = localAvg.ToString("0.0") + " ws/ps";
         }
-        catch (System.DivideByZeroException)
-        {
-
-        }
+        catch (System.DivideByZeroException) { }
     }
 }
