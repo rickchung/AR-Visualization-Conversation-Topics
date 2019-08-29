26f11b8df3abd788ad1189915547d773aa067e16
rickchung <rickjung88@gmail.com>
2019-08-29 10:35:24 -0700

    Use files for configurations

--- Assets/ConfManager.cs
+++ Assets/ConfManager.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ConfManager : MonoBehaviour
@@ -18,26 +20,35 @@ public class ConfManager : MonoBehaviour
     public GameObject arrowKeyPanel;
     public GameObject sttCtrlPanel;
     public GameObject developerPanel;
-    public GameObject startScreen;
-    public Button[] stageButtons;
+    public GameObject startScreen, endScreen;
+    public Transform stageProgressView;
+    public Button stageButtonPrefab;
+    private List<Button> stageButtons;
     private int currentStageIndex;
 
     [HideInInspector] public bool isSlave;
 
-    private Dictionary<string, OgStageConf> stages;
+    private Dictionary<string, OgStageConfig> stages;
 
     private void Start()
     {
         // Prevent the screen from sleeping
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        // Open the welcome screen
+        startScreen.SetActive(true);
+        endScreen.SetActive(false);
 
         // Load predefined maps/scripts to the data folder
         var dataFolderPath = Application.persistentDataPath;
         var filesToCopy = new string[] {
-            "OgMap-Tutorial1",
-            "OgMap-Tutorial2", "OgScript-Tutorial2-M", "OgScript-Tutorial2-S",
-            "OgMap-Puzzle1", "OgScript-Puzzle1-M", "OgScript-Puzzle1-S",
-            "OgMap-Puzzle2", "OgScript-Puzzle2-M", "OgScript-Puzzle2-S",
+            "OgConfig-Tutorial",
+                "OgMap-Tutorial1", "OgScript-Tutorial1-M", "OgScript-Tutorial1-S",
+            "OgConfig-Puzzle1",
+                "OgMap-Puzzle1", "OgScript-Puzzle1-M", "OgScript-Puzzle1-S",
+            "OgConfig-Puzzle2",
+                "OgMap-Puzzle2", "OgScript-Puzzle2-M", "OgScript-Puzzle2-S",
+            "OgConfig-Puzzle3",
+                "OgMap-Puzzle3", "OgScript-Puzzle3-M", "OgScript-Puzzle3-S",
         };
         foreach (var s in filesToCopy)
         {
@@ -53,69 +64,29 @@ public class ConfManager : MonoBehaviour
             );
         }
 
-        // Open the welcome screen
-        startScreen.SetActive(true);
-
         // Init configurations of stages
-
-        stages = new Dictionary<string, OgStageConf>();
-
-        var p1 = @"Tutorial
-
-Welcome! This is the first tutorial stage of Ogmented. In this tutorial, you will learn the interface of Ogmented including the layout of maps and how to navigate the avatar around.
-
-Your avater is a cube marked as blue on a grid. Please try to move your avatar around and collect the flag in the map.";
-
-        stages.Add("Tutorial1", new OgStageConf(
-            name: "Tutorial1",
-            problem: p1,
-            map: "OgMap-Tutorial1.txt",
-            masterScript: null,
-            slaveScript: null,
-            isArrowKeyEnabled: true,
-            isDeveloperPanelEnabled: false
-        ));
-
-        stages.Add("Puzzle1", new OgStageConf(
-            name: "Puzzle1",
-            problem: @"Puzzle 1",
-            map: "OgMap-Tutorial2.txt",
-            masterScript: "OgScript-Tutorial2-M.txt",
-            slaveScript: "OgScript-Tutorial2-S.txt",
-            isArrowKeyEnabled: false,
-            isDeveloperPanelEnabled: false
-        ));
-
-        stages.Add("Puzzle2", new OgStageConf(
-            name: "Puzzle2",
-            problem: @"Puzzle 2",
-            map: "OgMap-Puzzle1.txt",
-            masterScript: "OgScript-Puzzle1-M.txt",
-            slaveScript: "OgScript-Puzzle1-S.txt",
-            isArrowKeyEnabled: false,
-            isDeveloperPanelEnabled: false
-        ));
-
-        stages.Add("Puzzle3", new OgStageConf(
-            name: "Puzzle3",
-            problem: @"Puzzle 3",
-            map: "OgMap-Puzzle2.txt",
-            masterScript: "OgScript-Puzzle2-M.txt",
-            slaveScript: "OgScript-Puzzle2-S.txt",
-            isArrowKeyEnabled: false,
-            isDeveloperPanelEnabled: false
-        ));
-
+        stages = new Dictionary<string, OgStageConfig>();
+        stages.Add("Tutorial", OgStageConfig.ImportConfigFile("OgConfig-Tutorial"));
+        stages.Add("Puzzle1", OgStageConfig.ImportConfigFile("OgConfig-Puzzle1"));
+        stages.Add("Puzzle2", OgStageConfig.ImportConfigFile("OgConfig-Puzzle2"));
+        stages.Add("Puzzle3", OgStageConfig.ImportConfigFile("OgConfig-Puzzle3"));
 
         // Init stage buttons
-        if (stageButtons != null)
+        stageButtons = new List<Button>();
+        foreach (var kv in stages)
         {
-            foreach (var b in stageButtons)
-            {
-                b.interactable = false;
-            }
-            stageButtons[0].interactable = true;
+            var newButton = Instantiate(stageButtonPrefab, parent: stageProgressView);
+            var newButtonText = newButton.GetComponentInChildren<Text>();
+
+            newButton.onClick.AddListener(() => { ApplyConfiguration(kv.Key); });
+            newButton.interactable = false;
+            newButtonText.text = kv.Key;
+            newButton.gameObject.SetActive(true);
+
+            stageButtons.Add(newButton);
         }
+        stageButtons[0].interactable = true;
+        stageButtons[stageButtons.Count - 1].GetComponent<Image>().sprite = null;
     }
 
     public void ApplyConfiguration(string confName)
@@ -124,9 +95,13 @@ Your avater is a cube marked as blue on a grid. Please try to move your avatar a
         {
             ApplyConfiguration(stages[confName]);
         }
+        else
+        {
+            Debug.LogWarning("Configuration is not found: " + confName);
+        }
     }
 
-    public void ApplyConfiguration(OgStageConf conf)
+    public void ApplyConfiguration(OgStageConfig conf)
     {
         DataLogger.Log(this.gameObject, LogTag.SYSTEM, "Loading a configuration, " + conf);
         informationPanel.ReplaceContent(conf.problem);
@@ -150,20 +125,30 @@ Your avater is a cube marked as blue on a grid. Please try to move your avatar a
 
     public void StartGame()
     {
-        ApplyConfiguration("Tutorial1");
+        ApplyConfiguration("Tutorial");
         startScreen.SetActive(false);
+    }
+
+    public void StopGame()
+    {
+        endScreen.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void EnableNextStage()
     {
         currentStageIndex++;
-        if (stageButtons != null && currentStageIndex < stageButtons.Length)
+        if (stageButtons != null && currentStageIndex < stageButtons.Count)
         {
             stageButtons[currentStageIndex].interactable = true;
         }
     }
 
-    public class OgStageConf
+    public class OgStageConfig
     {
         public string name;
         public string problem;
@@ -171,18 +156,57 @@ Your avater is a cube marked as blue on a grid. Please try to move your avatar a
         public string masterScript, slaveScript;
         public bool isArrowKeyEnabled, isDeveloperPanelEnabled;
 
-        public OgStageConf(
+        public OgStageConfig(
             string name, string problem, string map, string masterScript, string slaveScript,
-            bool isArrowKeyEnabled, bool isDeveloperPanelEnabled
-        )
+            bool isArrowKeyEnabled, bool isDeveloperPanelEnabled)
         {
             this.name = name;
             this.problem = problem;
             this.map = map;
-            this.masterScript = masterScript;
-            this.slaveScript = slaveScript;
+            this.masterScript = masterScript.Equals("null") ? null : masterScript;
+            this.slaveScript = slaveScript.Equals("null") ? null : slaveScript;
             this.isArrowKeyEnabled = isArrowKeyEnabled;
             this.isDeveloperPanelEnabled = isDeveloperPanelEnabled;
+        }
+
+        public static OgStageConfig ImportConfigFile(string filename)
+        {
+            var dataDirPath = Application.persistentDataPath;
+            var filePath = Path.Combine(dataDirPath, filename) + ".txt";
+            OgStageConfig rt = null;
+
+            using (var reader = new StreamReader(filePath))
+            {
+                // L1: name
+                var name = reader.ReadLine();
+                // L2: problem desc
+                string problem = "";
+                string nextProblemLine = reader.ReadLine();
+                while (nextProblemLine != null && (!nextProblemLine.Trim().Equals("#")))
+                {
+                    problem += nextProblemLine + "\n";
+                    nextProblemLine = reader.ReadLine();
+                }
+
+                // L3: map filename (must be placed in Resources)
+                var map = reader.ReadLine();
+                // L4: master's script filename (must be placed in Resources)
+                var masterScript = reader.ReadLine();
+                // L5: slave's script filename (must be placed in Resources)
+                var slaveScript = reader.ReadLine();
+                // L6: Will the arrow keys be enabled?
+                var isArrowKeyEnabled = Boolean.Parse(reader.ReadLine());
+                // L7: Will the developer panel be enabled?
+                var isDeveloperPanelEnabled = Boolean.Parse(reader.ReadLine());
+
+                rt = new OgStageConfig(
+                    name, problem, map,
+                    masterScript, slaveScript,
+                    isArrowKeyEnabled, isDeveloperPanelEnabled
+                );
+            }
+
+            return rt;
         }
 
         override public string ToString()
